15a3ecd307527afad292188ca58dbf0208afe7e3
BosEriko <bos@eriko.ph>
2026-03-29 14:57:20 +0800

    Feature: Incorporate Atomic Design to NextJS default Page

--- app/(view)/organisms/Header/index.tsx
+++ app/(view)/organisms/Header/index.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header>
-      Header
+      <Image
+        className="dark:invert"
+        src="/next.svg"
+        alt="Next.js logo"
+        width={100}
+        height={20}
+        priority
+      />
     </header>
   );
 };
--- app/(view)/templates/Default/index.tsx
+++ app/(view)/templates/Default/index.tsx
@@ -8,10 +8,14 @@ interface DefaultProps {
 
 const Default: React.FC<DefaultProps> = ({ children }) => {
   return (
-    <div>
-      <Organism.Header />
-      <div>{children}</div>
-      <Organism.Footer />
+    <div className="min-h-screen min-w-screen flex flex-col">
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <Organism.Header />
+          {children}
+          <Organism.Footer />
+        </main>
+      </div>
     </div>
   );
 };
