45207fdc2fbf3764a5f6140178c6785656a63f72
BosEriko <bos@eriko.ph>
2026-04-01 02:55:35 +0800

    Feature: Use FirebaseTimestamp Type

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,4 @@
 import FirebaseAdmin from "@lib/FirebaseAdmin";
+import Nails from "core-nails";
 import { z } from "zod";
 
-// Helper Function
-const FirebaseTimestamp = z.union([
-  z.instanceof(FirebaseAdmin.firestore.Timestamp),
-  z.date(),
-  z.custom((val) => val === FirebaseAdmin.firestore.FieldValue.serverTimestamp(), { message: "Expected serverTimestamp()" }),
-]);
